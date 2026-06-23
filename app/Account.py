74b966b1eba1f0e8751e96e9b0f2a2ab626a1da8
surjanthakur <tsurjan506@gmail.com>
2026-06-24 02:31:53 +0530

    s

--- app/Account.py
+++ app/Account.py
@@ -3,12 +3,13 @@ import random
 
 
 class Account:
-    def __init__(self):
+    def __init__(self, owner: str, owner_id: str, account_type: str):
         self.account_number: int = random.randint(1000000000, 9999999999)
-        self.owner: str = "User"
+        self.owner: str = owner
+        self.owner_id: str = owner_id
         self.__balance: int = 0
         self.account_status: str = "active"
-        self.account_type: str = "saving"
+        self.account_type: str = account_type
         self.created_at: str = datetime.now().strftime("%d-%m-%Y")
 
     def deposite(self, amount: int):
--- app/Bank.py
+++ app/Bank.py
@@ -14,19 +14,54 @@ class Bank:
         self.transactions: set[Transaction] = {}
 
     def add_customer(self, customer: Customer):
-        pass
+        """
+        Docstring for add_customer
+
+        :param self: accept's current Customer object that is named self
+        :param customer: accept's a customer object
+        """
+        self.customers.add(customer)
+        return "new customer added"
 
     def remove_customer(self, customer_id: str):
-        pass
+        """
+        Docstring for remove_customer
 
-    def add_employee(self):
-        pass
+        :param self: accept's current Customer object that is named self
+        :param customer_id: accept's a customer_id string
+        """
+
+        cstmr = next((c for c in self.customers if c.customer_id == customer_id), None)
+        if cstmr is None:
+            return "wrong customer check again"
+        self.customers.remove(cstmr)
+        return "customer deleted successfully"
+
+    def add_employee(self, employee: Employee):
+        """
+        Docstring for add_employee
+
+        :param self: accept's current Employee object that is named self
+        :param employee: accept's employee object
+        """
+
+        self.employees.add(employee)
+        return "new employee added"
 
     def create_account(self, customer: Customer):
+        # check if customer exists or not
+        # check if account exists with the customer id
+        # create a new Account object
+        # update the accounts set
+        # return message
         pass
 
-    def remove_account(self, customer: Customer, account_no: int):
+    def remove_account(self, customer_id: str, account_no: int):
+        # verify the customer id and the account number match in the accounts object
+        # if match delete account else return error
         pass
 
-    def view_account(self, account_no: int):
+    def view_account(self, customer_id: str, account_no: int):
+        # verify the customer id and the account number match in the accounts object
+        # if match return info else error msg
         pass
